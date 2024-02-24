000802161772218f4a3e93decb2562e46d34eeb2
kyhtonllk <kythonlk@gmail.com>
2024-02-24 14:54:03 +0400

    Update little

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "ExtracoAE";
+export const SITE_TITLE = "Kythonlk";
 export const SITE_DESCRIPTION = "Welcome to my website!";
