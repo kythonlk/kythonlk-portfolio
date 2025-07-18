4c1547e48a7ba7fe744fe47abe4897175839ad42
kythonlk <kythonlk@gmail.com>
2025-07-18 16:05:14 +0400

    Update domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kythonlk.com',
+  site: 'https://kythonlk.tech',
   integrations: [mdx(), sitemap(), tailwind(), react()]
 });
